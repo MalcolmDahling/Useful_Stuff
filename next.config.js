5516830b60d5696702f400537b5a974c8f376e72
MalcolmDahling <92363535+MalcolmDahling@users.noreply.github.com>
2023-09-03 19:29:59 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental:{
+        appDir:false
+    },
+};
 
 module.exports = withVanillaExtract(nextConfig);
